1187c812a1ef530fef59131ba7b360f155f6d500
zackthomas1 <31631098+zackthomas1@users.noreply.github.com>
2024-12-07 19:02:13 -0500

    Create assert macro.

--- ToyEngine/src/ToyEngine/core.h
+++ ToyEngine/src/ToyEngine/core.h
@@ -1,7 +1,13 @@
 #pragma once
 
-#ifdef TY_BUILD_DLL
-	#define TOYENGINE_API __declspec(dllexport)
+#ifdef TY_DEBUG
+	#define TY_ENABLE_ASSERTS
+#endif // TY_DEBUG
+
+#ifdef TY_ENABLE_ASSERTS
+	#define TY_ASSERT(x, ...) { if(!(x)) { TY_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define TY_CORE_ASSERT(x, ...) { if(!(x)) { TY_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 #else
-	#define TOYENGINE_API __declspec(dllimport)
+	#define TY_ASSERT(x, ...)
+	#define TY_CORE_ASSERT(x, ...)
 #endif
