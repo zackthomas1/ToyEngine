315d4bf748f9c38bc11f2a3da4b8cf8a20eee00a
zackthomas1 <31631098+zackthomas1@users.noreply.github.com>
2025-07-05 18:01:16 -0700

    Create flat color shader and flat texture shader

---
+++ assets/shaders/flat_color.fs
@@ -0,0 +1,22 @@
+#version 330 core
+
+in vec2 texCoords;
+
+struct Material{
+
+    // Note: sampler2D is a so called opaque type which means 
+    // it be can't instantiated, but only defined as uniforms. 
+    // If the struct would be instantiated other than as a uniform 
+    // (like a function parameter) GLSL could throw strange errors; 
+    // the same thus applies to any struct holding such opaque types.
+    vec3 diffuse;
+
+}; uniform Material material;
+
+out vec4 FragColor;
+
+void main()
+{
+    vec3 diffuseColor = diffuse.rgb;
+    FragColor = vec4(diffuseColor, 1.0);
+}
---
+++ assets/shaders/flat_texture.vs
@@ -0,0 +1,18 @@
+#version 330 core
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
+
+uniform mat4 uModel;
+uniform mat4 uView;
+uniform mat4 uProjection;
+
+out vec2 texCoords;
+
+void main()
+{
+    // texture coordinates
+    texCoords = aTexCoords;
+    gl_Position = uProjection * uView * uModel * vec4(aPos, 1.0);
+}
