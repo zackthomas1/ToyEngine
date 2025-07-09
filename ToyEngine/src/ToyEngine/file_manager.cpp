757305f09115c9834b50c82df4cc3d94a0f18411
zackthomas1 <zackthomas13@gmail.com>
2025-07-09 15:34:22 -0700

    Add FileManager utility for reading source files
    Introduces the FileManager class with a static method to read the contents of a file into a string. This utility will help in loading source files, such as shaders, and includes error handling for file read failures.

---
+++ ToyEngine/src/ToyEngine/file_manager.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "file_manager.h"
+
+namespace ToyEngine
+{
+    std::string FileManager::ReadSourceFile(const char* path)
+    {
+		// retrieve source code from filePath
+		std::string source_code;
+		std::ifstream source_file;
+
+		// ensure ifstream objects can throw exceptions
+		source_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+		try
+		{
+			// open files
+			source_file.open(path);
+
+			// read file's buffer contents into streams 
+			std::stringstream source_stream;
+			source_stream << source_file.rdbuf();
+
+			// close file handlers 
+			source_file.close();
+
+			// convert stream into string 
+			source_code = source_stream.str();
+		}
+		catch (std::ifstream::failure e)
+		{
+			TY_CORE_ERROR("ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ");
+		}
+		return source_code;
+	}
+}
