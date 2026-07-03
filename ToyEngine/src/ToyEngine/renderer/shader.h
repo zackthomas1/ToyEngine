c1fd05dbb7619b3b3004f1217feaad42b76dd35f
zackthomas1 <zackthomas13@gmail.com>
2026-07-03 10:14:09 -0700

    Remove unused ShaderLibrary class. Its functionality has been depedicated for generalized ResourceManager.

--- ToyEngine/src/ToyEngine/renderer/shader.h
+++ ToyEngine/src/ToyEngine/renderer/shader.h
@@ -11,9 +11,9 @@ namespace ToyEngine
     virtual ~Shader() {}
 
     virtual void Use() = 0;
-    virtual void BindUniformBlock(const char* uniform_block, uint32_t binding_point) const = 0; 
+    virtual void BindUniformBlock(const std::string& uniform_block, uint32_t binding_point) const = 0;
 
-    virtual void SetBool(const std::string& name, bool value) const = 0; 
+    virtual void SetBool(const std::string& name, bool value) const = 0;
     virtual void SetInt(const std::string& name, int value) const = 0;
     virtual void SetFloat(const std::string& name, float value) const = 0;
     virtual void SetFloat2(const std::string& name, float v0, float v1) const = 0;
@@ -21,29 +21,16 @@ namespace ToyEngine
     virtual void SetFloat4(const std::string& name, float v1, float v2, float v3, float v4) const = 0;
     virtual void SetMat4(const std::string& name, glm::mat4 value) const = 0;
 
+    /// @brief Returns the unique identifier of the shader program.
+    /// @return The unique identifier of the shader program.
     uint32_t id() const { return id_; }
-    const char* GetName() const { return m_name; }
+    std::string GetName() const { return name_; }
 
     static Ref<Shader> Create(const char* shader_name, const char* vertex_path, const char* fragment_path);
-  public:
-    const char* m_name;
   protected:
     // constructor reads and builds the shader
-    Shader(const char* name) : m_name(name) {}
+    Shader(const std::string& name) : name_(name) {}
     uint32_t id_;
+    std::string name_;
   };
-
-    // ShaderLibrary manages a collection of Shader objects, allowing shaders to be loaded and retrieved by name.
-    // It stores shaders in an unordered_map for efficient lookup and ensures that each shader is uniquely identified by its name.
-    class ShaderLibrary
-    {
-    public:
-    ShaderLibrary() {}
-    ~ShaderLibrary() {}
-
-    void Add(Ref<Shader> shader);
-    Ref<Shader> Get(const std::string& name);
-    private:
-    std::unordered_map<std::string, Ref<Shader>> library_;
-    };
 }
